08e409306a3bd2717de84e913c72ad0d84baca99
shivaraj behere <shiva.june16@gmail.com>
2025-04-14 02:13:24 +0200

    add index file

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Wawibox\Product\ProductPack;
+use Wawibox\Order\Order;
+use Wawibox\Order\OrderItem;
+use Wawibox\Supplier\Supplier;
+use Wawibox\PriceCalculator\PriceCalculator;
+use Wawibox\Service\SupplierComparisonService;
+
+$supplierA = new Supplier('Supplier A', [
+    new ProductPack('Dental Floss', 1, 9.00),
+    new ProductPack('Dental Floss', 20, 160.00),
+    new ProductPack('Ibuprofen', 1, 5.00),
+    new ProductPack('Ibuprofen', 10, 48.00),
+]);
+
+$supplierB = new Supplier('Supplier B', [
+    new ProductPack('Dental Floss', 1, 8.00),
+    new ProductPack('Dental Floss', 10, 71.00),
+    new ProductPack('Ibuprofen', 1, 6.00),
+    new ProductPack('Ibuprofen', 5, 25.00),
+    new ProductPack('Ibuprofen', 100, 410.00),
+]);
+
+$order = new Order([
+    new OrderItem('Dental Floss', 5),
+    new OrderItem('Ibuprofen', 12),
+]);
+
+$service = new SupplierComparisonService(
+    [$supplierA, $supplierB],
+    new PriceCalculator()
+);
+
+$result = $service->findBestSupplier($order);
+
+echo "Best Supplier: " . $result['supplier']->getName() . PHP_EOL;
+echo "Total Price: " . number_format($result['price'], 2) . " EUR" . PHP_EOL;
